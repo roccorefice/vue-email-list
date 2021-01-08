8f1985cf3b10408cb0b4f7c24218538a7034473d
Rocco Orefice <rocco.orefice92@gmail.com>
2021-01-08 16:13:32 +0100

    finito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,20 @@
+// Istruzioni:
+// Genera 10 mail tramite api e stampale in una lista
+
+
 let app = new Vue ({
     el: '#app',
     data: {
         list: []
     },
     mounted(){
+        const _self = this;
         for (let i = 0; i < 10; i++) {
             axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
             .then(function(response){
                 // console.log(response.data.response);
                 randomEmail = response.data.response;
-                this.list.push(randomEmail)
-    
+                _self.list.push(randomEmail)
             });
         }
     }
